f06b2539f1ff2d0601c0f6903865427d7dbe092d
root <root@ip-172-31-31-138.ec2.internal>
2019-09-21 08:36:55 +0000

    aws cli file

---
+++ aws-cli.sh
@@ -0,0 +1,1 @@
+aws ec2 create-instances --instance-type t2.micro --count 1 --key-name HostnameKeyPair --subnet-id subnet-a33d06e9 --image-id ami-0b69ea66ff7391e80 --security-group-ids sg-0b2e1c79337a00442
