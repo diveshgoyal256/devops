2ae20e1f5e52ea80047afc35bd1238f075dcbb4c
root <root@ip-172-31-31-138.ec2.internal>
2019-09-21 08:52:43 +0000

    add region

--- aws-cli.sh
+++ aws-cli.sh
@@ -1,2 +1,2 @@
 # comment
-aws ec2 run-instances --instance-type t2.micro --count 1 --key-name HostnameKeyPair --subnet-id subnet-a33d06e9 --image-id ami-0b69ea66ff7391e80 --security-group-ids sg-0b2e1c79337a00442
+aws ec2 run-instances --instance-type t2.micro --count 1 --key-name HostnameKeyPair --subnet-id subnet-a33d06e9 --image-id ami-0b69ea66ff7391e80 --security-group-ids sg-0b2e1c79337a00442 --region us-east-1
